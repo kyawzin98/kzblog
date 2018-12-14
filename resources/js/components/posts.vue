ef650781308323d0187a3620742be78c787e943e
Kyaw Zin <immortalityskill2398@gmail.com>
2018-12-14 09:45:47 +0630

    Add like button

---
+++ resources/js/components/posts.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post-preview">
+        <a href="slug">
+            <h2 class="post-title">{{title}}</h2>
+            <h3 class="post-subtitle">
+                {{subtitle}}
+            </h3>
+        </a>
+        <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            {{created_at}}
+            <a href="" class="btn btn-link">
+                <small>0</small>
+                <i class="fa fa-thumbs-up"></i>
+            </a>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[
+            'title',
+        ]
+    }
+</script>
--- resources/views/user/blog.blade.php
+++ resources/views/user/blog.blade.php
@@ -1,4 +1,11 @@
 @extends('user.app')
+@section('style')
+    <style>
+        .fa-thumbs-up:hover{
+            color: red;
+        }
+    </style>
+@endsection
 @section('bg-img',asset('user/img/home-bg.jpg'))
 @section('title','Clean Blog')
 @section('subheading','Design by Start Bootstrap')
@@ -12,18 +19,24 @@
                         <a href="{{route('post',$post->slug)}}">
                             <h2 class="post-title">{{$post->title}}</h2>
                             <h3 class="post-subtitle">
-                               {{$post->subtitle}}
+                                {{$post->subtitle}}
                             </h3>
                         </a>
                         <p class="post-meta">Posted by
                             <a href="#">Start Bootstrap</a>
-                            {{$post->created_at->diffForHumans()}}</p>
+                            {{$post->created_at->diffForHumans()}}
+                            <a href="" class="btn btn-link">
+                                <small>0</small>
+                                <i class="fa fa-thumbs-up"></i>
+                            </a>
+                        </p>
                     </div>
+
                 @endforeach
                 <hr>
                 <!-- Pager -->
                 <div class="clearfix">
-                   <div class="">{{$posts->links()}}</div>
+                    <div class="">{{$posts->links()}}</div>
                     {{--<a class="btn btn-primary float-right" href="">Older Posts &rarr;</a>--}}
                 </div>
             </div>
